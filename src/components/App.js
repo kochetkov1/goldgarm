6ed13501fe71b603a1242e62bab3954146f0ffa0
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2022-10-13 16:59:25 +0400

    fix: App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import Сontacts from "./Сontacts";
-
+// import Сontacts from "./Сontacts";
 
 function App() {
-
   return (
-      <div className="App">
-        <div className="page">
-          <Header/>
+    <div className="App">
+      <div className="page">
+        <Header />
 
-          <Routes>
-            <Route
-              path="/"
-              element={
-                  <Main
-                  />
-              }
-            />
+        <Main />
 
-            <Route
-              path="/contacts"
-              element={<Сontacts/>}
-            />
-
-          </Routes>
-
-          <Footer />
-
-        </div>
+        <Footer />
       </div>
+    </div>
   );
 }
 
