0a235ecf7e4bd3b0d80fa6dc71be28619a9f86e4
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2022-12-02 09:57:11 +0400

    fix: message logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,29 @@
 import logo from "../images/header-logo.svg";
 import React from "react";
 
-function Header() {
+import icontg from "../images/icon-tg.svg";
+import iconwhatsapp from "../images/icon-whatsapp.svg";
+import iconviber from "../images/icon-viber.svg";
 
+function Header() {
   return (
     <header className="header">
+      <div className="contacts__callbox contacts__callbox-header"></div>
+      <img className="header__logo" src={logo} alt="GoldGarm" />
+
       <div className="contacts__callbox contacts__callbox-header">
-          </div>
-      <img
-        className="header__logo"
-        src={logo}
-        alt="GoldGarm"
-      />
-      <div className="contacts__callbox contacts__callbox-header">
-            <p className="contacts__contact contacts__contact-header">+7 (906) 393-80-46</p>
-            <p className="contacts__contact contacts__contact-header">goldgarm@ya.ru</p>
-          </div>
+        <p className="contacts__contact contacts__contact-header">
+          +7 (906) 393-80-46
+        </p>
+        <p className="contacts__contact contacts__contact-header">
+          goldgarm@ya.ru
+        </p>
+        <div className="header__container">
+          <img className="contacts__icon contacts__icon-header" src={icontg} alt="Telegram" />
+          <img className="contacts__icon contacts__icon-header" src={iconwhatsapp} alt="WhatsApp" />
+          <img className="contacts__icon contacts__icon-header" src={iconviber} alt="Viber" />
+        </div>
+      </div>
     </header>
   );
 }
